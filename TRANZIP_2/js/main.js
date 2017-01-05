3c414ec46f6f3bc6e0a39009bbfc2975a00c3782
Kaleb Davenport <kaleb@wowyourclient.com>
2017-01-05 15:39:11 -0500

    Add files via upload

---
+++ TRANZIP_2/js/main.js
@@ -0,0 +1,46 @@
+(function(){
+
+    // Menu settings
+    $("#menuToggle, .menu-close").on('click', function(){
+        $('#menuToggle, .menu-close').toggleClass('open');
+        if ($( window ).width() < 992) {
+            $('#scrollDownPanel, #reSearchPanel, #resultsPanel, .resultsNav, #scrollDownPanel, .mobileOpenUpPanel,' +
+                ' .mobileOpenDownPanel').toggleClass('lower');
+        }
+        $('body').toggleClass('body-push-toleft');
+        $('#theMenu').toggleClass('menu-open');
+        $('#search').toggleClass('hidden-sm');
+    });
+
+
+    // Modal Settings
+    $('#search').bind('keypress', function(e) {
+        if(e.keyCode==13){
+            e.preventDefault();
+            $('#myModal').modal('toggle');
+        }
+    });
+    $('.searchButton').bind('click', function(e) {
+        e.preventDefault();
+        $('#myModal').modal('toggle');
+    });
+
+    $('#scrollDownImg').bind('click', function(e) {
+        e.preventDefault();
+        $("#mobileTogglePanel").toggleClass("mobileOpenUpPanel mobileOpenDownPanel");
+        $("#mobileControls").fadeToggle();
+        $("#overlay").toggleClass('visible-xs hidden-xs');
+        $("#toggles").fadeToggle();
+        if ($('.desktopNav').is(":visible")) {
+            $(".desktopNav").fadeToggle(function () {
+                $(".mobileNav").fadeToggle("fast")
+            });
+        } else {
+            $(".mobileNav").fadeToggle(function () {
+                $(".desktopNav").fadeToggle("fast")
+            });
+        }
+    });
+
+})(jQuery);
+
